d092b64f915d644c991f07ca07569b78d426fc6b
tjololo <1145298+tjololo@users.noreply.github.com>
2024-02-28 07:59:01 +0100

    update entrypoint script

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,7 +3,7 @@
 # Retrieve a short lived runner registration token using the PAT
 REGISTRATION_TOKEN="$(curl -X POST -fsSL \
   -H 'Accept: application/vnd.github.v3+json' \
-  -H "Authorization: Bearer $GITHUB_PAT" \
+  -H "Authorization: Bearer $GITHUB_TOKEN" \
   -H 'X-GitHub-Api-Version: 2022-11-28' \
   "$REGISTRATION_TOKEN_API_URL" \
   | jq -r '.token')"
