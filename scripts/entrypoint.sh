f7b511d52984131694b9e5bbd987b666cd002dc8
tjololo <1145298+tjololo@users.noreply.github.com>
2024-08-15 12:04:17 +0200

    test jit runner

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -25,6 +25,8 @@ echo "Obtaining access token for app_id ${APP_ID} and login ${APP_LOGIN}"
 
 ACCESS_TOKEN=$(APP_ID="${APP_ID}" APP_PRIVATE_KEY="${APP_PRIVATE_KEY//\\n/${nl}}" APP_LOGIN="${APP_LOGIN}" bash ./app-token.sh)
 
+
+
 # Retrieve a short lived runner registration token using the PAT
 _TOKEN=$(ACCESS_TOKEN="${ACCESS_TOKEN}" bash ./token.sh)
 RUNNER_TOKEN=$(echo "${_TOKEN}" | jq -r .token)
@@ -38,5 +40,11 @@ echo "Configuring"
   --work "${_RUNNER_WORKDIR}" \
   --name "${_RUNNER_NAME}" \
   --unattended \
-  --ephemeral \
-  && ./run.sh
+  --ephemeral
+
+if [[ -n "JIT_RUNNER" ]]; then
+  JIT_CONFIG=$(REPO_URL="${REPO_URL}" RUNNER_NAME="${_RUNNER_NAME}" LABELS="${_LABELS}" WORK_FOLDER="${_RUNNER_WORKDIR}" bash ./jit-config.sh)
+  ./run.sh --jitconfig "${JIT_CONFIG}"
+else
+  ./run.sh
+fi
---
+++ scripts/jit-config.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#######
+# Input variables:
+# REPO_URL url to github repo (https://github.com/owner/repo)
+# RUNNER_NAME name of the runner
+# LABELS runner labels
+# WORK_FOLDER work folder for the runner
+
+API_VERSION=v3
+ACCEPT_HEADER="Accept: application/vnd.github.${API_VERSION}+json"
+AUTH_HEADER="Authorization: token ${ACCESS_TOKEN}"
+VERSION_HEADER="X-GitHub-Api-Version: 2022-11-28"
+
+REPO=$(basename "$REPO_URL" ".${REPO_URL##*.}")
+url_without_repo=$(echo "${REPO_URL/$REPO/}")
+OWNER=$(basename "$url_without_repo" ".${url_without_repo##.}")
+FULL_API_URL="https://api.github.com/repos/${OWNER}/${REPO}/actions/runners/generate-jitconfig"
+
+JSON_LABELS=$(jq -c -n --arg str "${LABELS}" '$str|split(",")')
+REQUEST_BODY="{\"name\":\"$RUNNER_NAME\",\"runner_group_id\":\"1\",\"labels\":${JSON_LABELS},\"work_folder\":\"${WORK_FOLDER}}"
+
+
+echo $REPO $OWNER
+JIT_CONFIG=$(curl -L -XPOST \
+    -H ${ACCEPT_HEADER} \
+    -H ${AUTH_HEADER} \
+    -H ${VERSION_HEADER} \
+    "${FULL_API_URL}"
+    -d "${REQUEST_BODY}"
+)
+
+echo ${JIT_CONFIG} | base64
