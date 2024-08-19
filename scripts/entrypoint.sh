a3d4c3fdbc0175b7c7fc5b0d95a4cc6fd32c61ab
tjololo <1145298+tjololo@users.noreply.github.com>
2024-08-19 09:35:03 +0200

    test unset of env vars

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -11,6 +11,7 @@ _RUNNER_NAME=${RUNNER_NAME:-${RUNNER_NAME_PREFIX:-github-runner}-$(head /dev/ura
 _RUNNER_WORKDIR=${RUNNER_WORKDIR:-/_work/${_RUNNER_NAME}}
 _LABELS=${LABELS:-default}
 _RUNNER_GROUP=${RUNNER_GROUP:-Default}
+_RUNNER_GROUP_ID=${RUNNER_GROUP_ID:-1}
 
 [[ -z ${REPO_URL} ]] && ( echo "REPO_URL required for repo runners"; exit 1 )
 _SHORT_URL=${REPO_URL}
@@ -26,17 +27,28 @@ echo "Obtaining access token for app_id ${APP_ID} and login ${APP_LOGIN}"
 ACCESS_TOKEN=$(APP_ID="${APP_ID}" APP_PRIVATE_KEY="${APP_PRIVATE_KEY//\\n/${nl}}" APP_LOGIN="${APP_LOGIN}" bash ./app-token.sh)
 
 
-
 # Retrieve a short lived runner registration token using the PAT
 _TOKEN=$(ACCESS_TOKEN="${ACCESS_TOKEN}" bash ./token.sh)
 RUNNER_TOKEN=$(echo "${_TOKEN}" | jq -r .token)
 
-echo "Configuring"
-
 if [[ -n "${JIT_RUNNER}" ]]; then
+  ./config.sh \
+    --url $REPO_URL \
+    --token $RUNNER_TOKEN \
+    --labels "${_LABELS}" \
+    --work "${_RUNNER_WORKDIR}" \
+    --name "${_RUNNER_NAME}" \
+    --runnergroup "${_RUNNER_GROUP}" \
+    --unattended \
+    --replace \
+    --ephemeral
   JIT_CONFIG=$(REPO_URL="${REPO_URL}" NAME="${_RUNNER_NAME}" LABELS="${_LABELS}" WORK_FOLDER="${_RUNNER_WORKDIR}" ACCESS_TOKEN="${ACCESS_TOKEN}" bash ./jit-config.sh)
-  echo -n "Starting runner with JIT config ${JIT_CONFIG}"
-  ENCODED_JIT_CONFIG=$(echo -n "${JIT_CONFIG}" | jq -r '.encoded_jit_config')
+  echo "Starting runner with JIT config ${JIT_CONFIG}"
+  ENCODED_JIT_CONFIG=$(jq -r '.encoded_jit_config' <<< "${JIT_CONFIG}")
+  unset ACCESS_TOKEN
+  unset RUNNER_TOKEN
+  unset APP_ID
+  unset APP_PRIVATE_KEY
   ./run.sh --jitconfig "${ENCODED_JIT_CONFIG}"
 else
   echo "Starting runner without JIT config"
@@ -46,7 +58,13 @@ else
     --labels "${_LABELS}" \
     --work "${_RUNNER_WORKDIR}" \
     --name "${_RUNNER_NAME}" \
+    --runnergroup "${_RUNNER_GROUP}" \
     --unattended \
+    --replace \
     --ephemeral
+  unset ACCESS_TOKEN
+  unset RUNNER_TOKEN
+  unset APP_ID
+  unset APP_PRIVATE_KEY
   ./run.sh
 fi
--- scripts/jit-config.sh
+++ scripts/jit-config.sh
@@ -7,8 +7,7 @@
 # NAME name of the runner
 # LABELS runner labels
 # WORK_FOLDER work folder for the runner
-
-echo "Fetching JIT config"
+# RUNNER_GROUP_ID id of the runner group this runner belongs to
 
 API_VERSION=v3
 ACCEPT_HEADER="Accept: application/vnd.github.${API_VERSION}+json"
@@ -21,15 +20,12 @@ OWNER=$(basename "$url_without_repo" ".${url_without_repo##.}")
 FULL_API_URL="https://api.github.com/repos/${OWNER}/${REPO}/actions/runners/generate-jitconfig"
 
 JSON_LABELS=$(jq -c -n --arg str "${LABELS}" '$str|split(",")')
-REQUEST_BODY="{\"name\":\"${NAME}\",\"runner_group_id\":1,\"labels\":${JSON_LABELS},\"work_folder\":\"${WORK_FOLDER}\"}"
+REQUEST_BODY="{\"name\":\"${NAME}\",\"runner_group_id\":${RUNNER_GROUP_ID},\"labels\":${JSON_LABELS},\"work_folder\":\"${WORK_FOLDER}\"}"
 
-echo JIT request body: ${REQUEST_BODY}
-
-JIT_CONFIG="$(curl -sSL -XPOST \
+JIT_CONFIG="$(curl -fsSL -XPOST \
     -H "${ACCEPT_HEADER}" \
     -H "${AUTH_HEADER}" \
     -H "${VERSION_HEADER}" \
     "${FULL_API_URL}" \
     -d "${REQUEST_BODY}")"
-#echo JIT config respons: ${JIT_CONFIG}
 echo "${JIT_CONFIG}"
