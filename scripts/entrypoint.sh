5d33a7662cb220c9be2bce0148165f6b191dad0a
tjololo <1145298+tjololo@users.noreply.github.com>
2024-08-26 11:48:54 +0200

    remove repo_url to simplify setup

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -4,8 +4,9 @@ export PATH=${PATH}:/actions-runner
 
 ##### ENVVARS
 # APP_ID ID of app used for registering and setting up runner
-# AP_PRIVATE_KEY Private key for app (should be kept in a vault)
-# REPO_URL Url to the repository. Example: https://github.com/tjololo/github-runner-keda
+# APP_PRIVATE_KEY Private key for app (should be kept in a vault)
+# ORG_NAME Name of the org the runner should be added to (For repo runners please supply REPO_NAME as well)
+# REPO_NAME (optional) Name of the repository to add this runner to. Leave unset for org runners
 # RUNNER_NAME (optional) Name of the runner. Default: ${RUNNER_NAME_PREFIX}-<random-string>
 # RUNNER_NAME_PREFIX (optional) The name will have random string add after the prefix. Default: github-runner
 # RUNNER_WORKDIR (optional) Work dir for the runner. Default: /_work/${RUNNER_NAME}
@@ -26,6 +27,7 @@ _RUNNER_WORKDIR=${RUNNER_WORKDIR:-/_work/${_RUNNER_NAME}}
 _LABELS=${LABELS:-default}
 _RUNNER_GROUP=${RUNNER_GROUP:-Default}
 _RUNNER_GROUP_ID=${RUNNER_GROUP_ID:-1}
+_BASE_HOST=${GITHUB_HOST:-"https://github.com"}
 
 ## Unset these, this may help prevent leaks
 unset_env() {
@@ -35,13 +37,18 @@ unset_env() {
   unset APP_PRIVATE_KEY
 }
 
-[[ -z ${REPO_URL} ]] && ( echo "REPO_URL required for repo runners"; exit 1 )
-_SHORT_URL=${REPO_URL}
-RUNNER_SCOPE="repo"
+[[ -z ${APP_ID} ]] && ( echo "APP_ID is required"; exit 1 )
+[[ -z ${APP_PRIVATE_KEY} ]] && (echo "APP_PRIVATE_KEY is required"; exit 1)
+[[ -z ${ORG_NAME} ]] && (echo "Atleast ORG_NAME is required, to define a Repo runner define REPO_NAME as well"; exit 1)
 
-if [[ -n "${APP_ID}" ]] && [[ -z "${APP_LOGIN}" ]]; then
-  APP_LOGIN=${REPO_URL%/*}
-  APP_LOGIN=${APP_LOGIN##*/}
+APP_LOGIN=${ORG_NAME}
+
+if [[ -z ${REPO_NAME }]]; then
+  _SHORT_URL="${_BASE_HOST}/${ORG_NAME}"
+  RUNNER_SCOPE="org"
+else
+  _SHORT_URL="${_BASE_HOST}/${ORG_NAME}/${REPO_NAME}"
+  RUNNER_SCOPE="repo"
 fi
 
 echo "Obtaining access token for app_id ${APP_ID} and login ${APP_LOGIN}"
@@ -55,7 +62,7 @@ RUNNER_TOKEN=$(echo "${_TOKEN}" | jq -r .token)
 
 if [[ -n "${JIT_RUNNER}" ]]; then
   ./config.sh \
-    --url $REPO_URL \
+    --url $_SHORT_URL \
     --token $RUNNER_TOKEN \
     --labels "${_LABELS}" \
     --work "${_RUNNER_WORKDIR}" \
@@ -64,7 +71,7 @@ if [[ -n "${JIT_RUNNER}" ]]; then
     --unattended \
     --replace \
     --ephemeral
-  JIT_CONFIG=$(REPO_URL="${REPO_URL}" NAME="${_RUNNER_NAME}" LABELS="${_LABELS}" WORK_FOLDER="${_RUNNER_WORKDIR}" ACCESS_TOKEN="${ACCESS_TOKEN}" bash ./jit-config.sh)
+  JIT_CONFIG=$(REPO_URL="${_SHORT_URL}" NAME="${_RUNNER_NAME}" LABELS="${_LABELS}" WORK_FOLDER="${_RUNNER_WORKDIR}" ACCESS_TOKEN="${ACCESS_TOKEN}" bash ./jit-config.sh)
   echo "Starting runner with JIT config ${JIT_CONFIG}"
   ENCODED_JIT_CONFIG=$(jq -r '.encoded_jit_config' <<< "${JIT_CONFIG}")
   unset_env
@@ -72,7 +79,7 @@ if [[ -n "${JIT_RUNNER}" ]]; then
 else
   echo "Starting runner without JIT config"
   ./config.sh \
-    --url $REPO_URL \
+    --url $_SHORT_URL \
     --token $RUNNER_TOKEN \
     --labels "${_LABELS}" \
     --work "${_RUNNER_WORKDIR}" \
